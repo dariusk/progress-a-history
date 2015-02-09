8897434326834a3408cadb671ca9aebd3206ea7c
Diana Thayer <garbados@gmail.com>
2015-02-09 02:49:05 -0800

    switch options

--- bin/index.js
+++ bin/index.js
@@ -9,15 +9,17 @@ program
 .description(pkg.description)
 .option('-j, --json', 'outputs raw JSON')
 .option('-r, --rules <rules>', 'specifies the ruleset. defaults to jaled', 'jaled')
-.option('-b, --basic', 'adds AI to brute-force different choices', false)
-.option('-i, --interactive', 'adds an AI the player controls directly', false)
+.option('-b, --basic', 'adds AI to brute-force different choices')
+.option('-a, --autonomous', 'removes the AI the player controls directly')
 .option('-t, --turns <turns>', 'number of turns in the game. defaults to 50', 50)
 .parse(process.argv);
+
+
 
 var rules = progress.rulesets[program.rules] || progress.rulesets.jaled;
 var players = progress.players[program.rules] || progress.players.jaled;
 var basic = program.basic && progress.players.common.make(rules.choices);
-var human = program.interactive && progress.players.common.repl;
+var human = !program.autonomous && progress.players.common.repl;
 
 progress.civ
 .rules(rules)
